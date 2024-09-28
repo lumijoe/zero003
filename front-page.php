9d9530f8684f56d822af0ff9c9f932f831c004c8
lumijoe <spacedesign63@gmail.com>
2024-09-28 12:30:30 +0900

    test

--- front-page.php
+++ front-page.php
@@ -28,15 +28,6 @@ get_header();
     </section>
 </main>
 
-<footer style="height: 400px; background-color: bisque;">
-    <section class="footer-first" style="background-color: aquamarine; height: 300px;">
-        <h2>Footer First Section</h2>
-    </section>
-    <section class="footer-second" style="background-color: rgb(255, 249, 127); height: 120px;">
-        <h2>Footer Second Section</h2>
-    </section>
-</footer>
-
 <?php
 get_footer(); // footer.php を読み込む
 ?>
