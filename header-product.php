8daa2628947b13bc60a30da143411c34af738982
lumijoe <spacedesign63@gmail.com>
2024-09-29 16:56:58 +0900

    test

--- header-product.php
+++ header-product.php
@@ -11,6 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="robots" content="noindex">
     <link rel="stylesheet" href="https://unpkg.com/ress@4.0.0/dist/ress.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <title>zero3 | 分野Aページ</title>
     <style>
         .product-list {
